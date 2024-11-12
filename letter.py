17fd39ce76543970e46f12137b85b4f77ec64439
ifacebob <ugochukwubobby@gmail.com>
2024-11-12 21:44:50 +0100

    A letter to my boss lady

---
+++ letter.py
@@ -0,0 +1,37 @@
+import datetime
+
+# special letter to my Boss Lady 
+letter = "Python Class \nAfternoon Season \nLearn Factory \nAba, Abia State \nNovember 12, 2024 \n"
+
+# I can decide to use write like this
+# letter += "iiuuuuy \n"
+# letter += ""
+# letter += ""
+# letter += "November 12, 2024 \n"
+
+print('\n')
+
+letter_body = "Dear Boss-Lady Cynthia,"
+
+instructor = "Cynthia"
+
+letter_subject = "Commendation Letter".center(120
+                                              ).upper()
+
+print('\n\n')
+
+letter_body += "I'm doing this letter to express my sincere gratitude me due to the approach you have been using to tutor me. When we started, It was as if you could not do it; but I insisted that we must to do teaching and learning together. I wanted you to guide me during **Tailwind CSS**. it wasn't that easy at the beginning, at time progresses, the class started becoming **interative and impressive**. \n\n"
+
+letter_body += "Personally dear, this our one-on-one class sessions has been particularly helpful to me. I can remember vividly, when I was passing through emotional trauma, this session through your worlds of encouragement gave me hope and comfort. Honestly your words and Sally words brought a new strength to my life. it gave me a renewed spirit. \n\n"
+
+letter_body += f"Now that we have transitioned to another level 'Python', I am equally enthusiastic about the learning process. I strongly believed with your 100% dedication and teaching support, I will definitely achieve my dream in a shorter time. Believe me dear," + instructor + "your guidance without bias will make this learnig journey enjoyable and effective. \n\n"
+
+letter_body += "Thank you for your dedication and support. \n\n"
+letter_body += "Best regards \n"
+letter_body += "Bobby Fasttrack"
+
+
+print(letter)
+print(letter_subject)
+print(letter_body)
+
