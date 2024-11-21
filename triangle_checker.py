873dfb4a685c8dd2716ea109ce36306357746386
ifacebob <ugochukwubobby@gmail.com>
2024-11-21 05:42:02 +0100

    Triangle by types

--- triangle_checker.py
+++ triangle_checker.py
@@ -10,7 +10,7 @@ if top + left_side <= Right_side or top + Right_side <= left_side or left_side +
   print('This is not a Valid Triangle')
   
 elif top == left_side and left_side == Right_side:
-  print("Equilateral Triangle: This is a Valid because their sizes are equal")
+  print("Equilateral Triangle: This is Valid because their sizes are equal")
 
 elif top == left_side or left_side == Right_side or top == Right_side:
   print("Isosceles Triangle: Two sides are equal ")
